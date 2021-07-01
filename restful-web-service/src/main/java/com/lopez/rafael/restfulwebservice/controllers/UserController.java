e6e33d92cf06d1a80295adcc3b0f75f5ea87a1d2
Rafael Lopez <lopez.rafael08@gmail.com>
2021-06-30 22:15:36 -0400

    RESTful Web Service: Add User model + service + controller

---
+++ restful-web-service/src/main/java/com/lopez/rafael/restfulwebservice/controllers/UserController.java
@@ -0,0 +1,51 @@
+package com.lopez.rafael.restfulwebservice.controllers;
+
+import com.lopez.rafael.restfulwebservice.models.User;
+import com.lopez.rafael.restfulwebservice.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("users")
+    public List<User> retrieveAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("users/{id}")
+    public User retrieveUser(@PathVariable Integer id) {
+        return userService.findOne(id);
+    }
+
+    @PostMapping("users")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        // We want to return the appropriate HTTP code for resources created => 201
+        // WHat's the URI of the new resource created?
+        User savedUser = userService.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("users/{id}")
+    public void deleteUser(@PathVariable Integer id){
+        userService.deleteById(id);
+    }
+}
---
+++ restful-web-service/src/main/java/com/lopez/rafael/restfulwebservice/services/UserService.java
@@ -0,0 +1,58 @@
+package com.lopez.rafael.restfulwebservice.services;
+
+import com.lopez.rafael.restfulwebservice.models.User;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class UserService {
+    private static List<User> users = new ArrayList();
+    private static int userCount = 3;
+
+    static {
+        users.add(new User(1, "Jack", LocalDate.now()));
+        users.add(new User(2, "Eve", LocalDate.now()));
+        users.add(new User(3, "Sam", LocalDate.now()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        if(user.getId() == null) {
+            user.setId(++userCount);
+        }
+
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id) {
+        for(User user: users) {
+            if(user.getId() == id) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public User deleteById(int id) {
+        Iterator<User> iterator = users.iterator();
+
+        while(iterator.hasNext()) {
+            User user = iterator.next();
+            if(user.getId() == id) {
+                iterator.remove();
+                return user;
+            }
+        }
+
+        return null;
+    }
+}
