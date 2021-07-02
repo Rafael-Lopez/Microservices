e9dcdcaaca38146954d9af5354ad480bf6724987
Rafael Lopez <lopez.rafael08@gmail.com>
2021-07-02 17:15:59 -0400

    Microservices & Spring Cloud: Enhance limits-service by getting configuration from application.properties

--- limits-service/src/main/java/com/lopez/rafael/limitsservice/controllers/LimitsController.java
+++ limits-service/src/main/java/com/lopez/rafael/limitsservice/controllers/LimitsController.java
@@ -1,14 +1,23 @@
 package com.lopez.rafael.limitsservice.controllers;
 
+import com.lopez.rafael.limitsservice.config.Configuration;
 import com.lopez.rafael.limitsservice.model.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    private Configuration configuration;
+
+    @Autowired
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
