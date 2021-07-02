ee6f4270fae895a553ca1312333b5a1e38a1f0e4
Rafael Lopez <lopez.rafael08@gmail.com>
2021-07-02 13:43:29 -0400

    Microservices & Spring Cloud: Add limits-service project + LimitsController

---
+++ limits-service/src/test/java/com/lopez/rafael/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopez.rafael.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
