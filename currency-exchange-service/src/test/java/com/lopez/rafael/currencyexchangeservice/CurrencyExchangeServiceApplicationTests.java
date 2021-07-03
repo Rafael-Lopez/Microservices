f875734aafe03aa115940b53f1cef5e993bc7950
Rafael Lopez <lopez.rafael08@gmail.com>
2021-07-03 16:52:56 -0400

    Microservices & Spring Cloud: Set up Currency Exchange Microservice

---
+++ currency-exchange-service/src/test/java/com/lopez/rafael/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopez.rafael.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
