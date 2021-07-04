90667de877947361f5652b5f64cb4eb6cd3f7d79
Rafael Lopez <lopez.rafael08@gmail.com>
2021-07-03 20:50:27 -0400

    Microservices & Spring Cloud: Load balancing with Eureka, Feign & Spring Cloud LoadBalancer

--- currency-conversion-service/src/main/java/com/lopez/rafael/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/lopez/rafael/currencyconversionservice/CurrencyExchangeProxy.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // For the name, typically you want to use the application name of the service you want to call
-// If you look at the appilcation.properties file for currency-exchange-service, the application name
+// If you look at the application.properties file for currency-exchange-service, the application name
 // defined there is "currency-exchange"
-@FeignClient(name = "currency-exchange", url = "http://localhost:8000")
+// @FeignClient(name = "currency-exchange", url = "http://localhost:8000")
+
+// Eureka does load balancing automatically for us. All we need to do is remove the 'url' property in @FeignClient
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
     @GetMapping("currency-exchange/from/{from}/to/{to}")
-    public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+    CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 }
