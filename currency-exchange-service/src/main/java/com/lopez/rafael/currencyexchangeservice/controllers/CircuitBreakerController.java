21744301cbd8221075fea4854e16947b55b8ab64
Rafael Lopez <lopez.rafael08@gmail.com>
2021-07-05 12:46:56 -0400

    Microservices & Spring Cloud: Use default Resilience4j's Retry strategy

---
+++ currency-exchange-service/src/main/java/com/lopez/rafael/currencyexchangeservice/controllers/CircuitBreakerController.java
@@ -0,0 +1,29 @@
+package com.lopez.rafael.currencyexchangeservice.controllers;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    // Resilience4j's default retry strategy is to try 3 times if there's an error (exception).
+    // Only after the third failure, it will return the actual error
+    @Retry(name = "default")
+    public String sampleApi() {
+        logger.info("Sample API call received");
+
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity(
+                "http://localhost:8080/some-dummy-call",
+                String.class
+        );
+
+        return forEntity.getBody();
+    }
+}
