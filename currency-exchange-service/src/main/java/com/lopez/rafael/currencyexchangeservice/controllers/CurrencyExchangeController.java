3cf2efb4a172638b511cff484d5760e4a49db495
Rafael Lopez <lopez.rafael08@gmail.com>
2021-07-05 17:38:56 -0400

    Microservices & Spring Cloud: Connect Currency Exchange microservice with Zipkin

--- currency-exchange-service/src/main/java/com/lopez/rafael/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/lopez/rafael/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.lopez.rafael.currencyexchangeservice.controllers;
 
 import com.lopez.rafael.currencyexchangeservice.models.CurrencyExchange;
 import com.lopez.rafael.currencyexchangeservice.services.CurrencyExchangeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,7 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
-
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     private Environment environment;
     private CurrencyExchangeService currencyExchangeService;
 
@@ -24,6 +26,8 @@ public class CurrencyExchangeController {
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
         CurrencyExchange currencyExchange = currencyExchangeService.findByFromAndTo(from, to);
         currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
 
