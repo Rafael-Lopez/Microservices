0bfb2f7047e2918db7ed21887585810f6da4d5ab
Rafael Lopez <lopez.rafael08@gmail.com>
2021-07-23 17:40:55 -0400

    KUBERNETES: Create environment variables to enable microservice communication

--- kubernetes-project/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ kubernetes-project/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 //@FeignClient(name="currency-exchange")
 
 //CHANGE-KUBERNETES
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
-//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")
+// This approach uses the CURRENCY_EXCHANGE_SERVICE_HOST default environment variable created by Kubernetes when deploying
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+// What happens if the exchange service is down when currency conversion is being deployed? There won't be
+// a CURRENCY_EXCHANGE_SERVICE_HOST variable. Therefore, this is not a good approach. It's better to use
+// a custom environment variable
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")
 public interface CurrencyExchangeProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
